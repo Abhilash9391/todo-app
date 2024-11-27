d6a561cfc7454613823a80e70400469be60b14ad
abhilash <abhi9391reddy@gmail.com>
2024-11-27 22:11:27 +0530

    adding

--- cloudfare.js
+++
@@ -1,8 +0,0 @@
-import {hono} from hono ;
-const app = hono()
-app.post('/',async (c)=>{
-    const body = await c.req.json;
-    console.log(c.req.header("Authorization"));
-    console.log(c.log.query("params"));
-    return c.text('hello hono!')
-})
